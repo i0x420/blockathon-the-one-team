853ed3fd2f37aca163f040d4c1b2d8d6b1a663d7
Endale98 <daida@coin98.finance>
2024-05-04 20:32:25 +0700

    add script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,3 +1,5 @@
+import { FeeManager } from "./../typechain-types/FeeManager";
+import { NESocial } from "./../typechain-types/NESocicial.sol/NESocial";
 import { writeFileSync } from "fs";
 import { ethers } from "hardhat";
 import { Helper } from "./helper";
@@ -21,11 +23,46 @@ const deployCollection = async (
   return contract;
 };
 
+async function deployFeeManager(
+  deployer: SignerWithAddress
+): Promise<FeeManager> {
+  const feeManager__factory = await ethers.getContractFactory("FeeManager");
+  const feeManager = await feeManager__factory
+    .connect(deployer)
+    .deploy(deployer.address);
+
+  return feeManager;
+}
+
+const deployNESocial = async (
+  deployer: SignerWithAddress,
+  feeManager: FeeManager,
+  collectionImpl: SocialCollection
+): Promise<NESocial> => {
+  const social__factory = await ethers.getContractFactory("NESocial");
+  const NESocial = await social__factory.deploy(feeManager.address);
+  console.log("NESocial", NESocial.address);
+
+  await NESocial.connect(deployer).setImplement(collectionImpl.address);
+  return NESocial;
+};
+
 const main = async () => {
   const [deployer] = await ethers.getSigners();
   console.info("Deployer address", deployer.address);
 
   const collection: SocialCollection = await deployCollection(deployer);
+  console.log("Collection", collection.address);
+
+  const feeManager: FeeManager = await deployFeeManager(deployer);
+  console.log("feeManager", feeManager.address);
+
+  const NESocial: NESocial = await deployNESocial(
+    deployer,
+    feeManager,
+    collection
+  );
+  console.log("feeManager", feeManager.address);
 
   await Helper.syncWriteFile(
     "./constants.ts",
