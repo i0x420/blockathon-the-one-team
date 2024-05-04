2be4aebac1e04a0a64759e5713f2a411e916b788
Endale98 <daida@coin98.finance>
2024-05-05 04:04:48 +0700

    update

--- contracts/MainToken.sol
+++ contracts/MainToken.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.19;
 import "./libraries/VRC25.sol";
 
 contract MainToken is VRC25 {
-    constructor(uint256 initialSupply) VRC25("Token A", "TA", 18, msg.sender) {
+    constructor(uint256 initialSupply) VRC25("Vibe", "Vibe", 18, msg.sender) {
         _mint(msg.sender, initialSupply);
     }
 
